3fa1f315d6b863e8d09f8ac673f5550721f6c3cb
bird <zeptomonkey@gmail.com>
2018-04-19 11:15:43 -0400

    start and heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ express = require('express');
 hbs = require('hbs');
 fs = require('fs');
 
+var port = process.env.PORT || 3000;
 app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -34,7 +35,7 @@ app.get('/about', function(req,res){
 	res.render('about.hbs');
 	});
 */
-app.listen(3000, function(){
-	console.log('listening on port 3000...');
+app.listen(port, function(){
+	console.log('listening on port...3000');
 	});
 
